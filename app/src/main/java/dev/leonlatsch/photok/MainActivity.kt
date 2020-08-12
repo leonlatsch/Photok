f30d3b5ff15319702670dadea0ff16641ed26a16
Leon Latsch <dev.leonlatsch@gmail.com>
2020-08-12 20:35:32 +0200

    initial commit

---
+++ app/src/main/java/dev/leonlatsch/photok/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.leonlatsch.photok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
