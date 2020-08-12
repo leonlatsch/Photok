c8e362d2bbd575f275bb279954b686c963e96e00
Leon Latsch <dev.leonlatsch@gmail.com>
2020-08-12 21:20:30 +0200

    add dagger hilt skeleton

---
+++ app/src/main/java/dev/leonlatsch/photok/BaseApplication.kt
@@ -0,0 +1,8 @@
+package dev.leonlatsch.photok
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
---
+++ app/src/main/java/dev/leonlatsch/photok/di/AppModule.kt
@@ -0,0 +1,10 @@
+package dev.leonlatsch.photok.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+}
