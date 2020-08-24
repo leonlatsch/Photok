9a34c7412a2eb34804bfbdd9e61e83d40ec257b6
Leon Latsch <dev.leonlatsch@gmail.com>
2020-08-24 17:35:42 +0200

    add text ui for passwords

--- app/src/main/java/dev/leonlatsch/photok/ui/setup/SetupFragment.kt
+++ app/src/main/java/dev/leonlatsch/photok/ui/setup/SetupFragment.kt
@@ -1,12 +1,34 @@
 package dev.leonlatsch.photok.ui.setup
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.leonlatsch.photok.R
+import dev.leonlatsch.photok.databinding.FragmentSetupBinding
 
 @AndroidEntryPoint
-class SetupFragment : Fragment(R.layout.fragment_setup) {
+class SetupFragment : Fragment() {
 
     private val viewModel: SetupViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentSetupBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_setup, container, false)
+        binding.viewModel = viewModel
+        binding.submitClickListener = submitClickListener
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    private val submitClickListener = View.OnClickListener {
+        println("Submit")
+    }
 }
