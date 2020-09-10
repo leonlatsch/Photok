ddd6abc3b3144ac338ed200ae24fb6b31b0dda5f
Leon Latsch <dev.leonlatsch@gmail.com>
2020-09-10 22:28:44 +0200

    small fixes

--- app/src/main/java/dev/leonlatsch/photok/ui/components/BaseActivity.kt
+++ app/src/main/java/dev/leonlatsch/photok/ui/components/BaseActivity.kt
@@ -8,6 +8,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
