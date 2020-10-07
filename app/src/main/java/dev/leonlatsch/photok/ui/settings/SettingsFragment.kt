03c8015f2a6b4de3085543adf4ffd9a36b36ef6d
Ismael <ismaelmelchorjuan@gmail.com>
2020-10-07 06:21:11 +0200

    Change Gallery Columns functionality

--- app/src/main/java/dev/leonlatsch/photok/ui/settings/SettingsFragment.kt
+++ app/src/main/java/dev/leonlatsch/photok/ui/settings/SettingsFragment.kt
@@ -16,12 +16,22 @@
 
 package dev.leonlatsch.photok.ui.settings
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.paging.Config
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.GridLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 import dev.leonlatsch.photok.R
+import dev.leonlatsch.photok.settings.Config
+import kotlinx.android.synthetic.main.fragment_gallery.*
 import kotlinx.android.synthetic.main.preference_layout_template.*
+import javax.inject.Inject
 
 /**
  * Preference Fragment. Loads preferences from xml resource.
@@ -29,10 +39,28 @@ import kotlinx.android.synthetic.main.preference_layout_template.*
  * @since 1.0.0
  * @author Leon Latsch
  */
+@AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    @Inject
+    lateinit var config: Config
+    var galeryColumns = 0
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        galeryColumns = config.getIntFromString(
+                Config.GALLERY_ADVANCED_GALLERY_COLUMNS,
+                Config.GALLERY_ADVANCED_GALLERY_COLUMNS_DEFAULT
+            )
+
+        val dialogGaleryCloumns =
+            findPreference("gallery^advanced.galleryColumns") as Preference?
+
+        dialogGaleryCloumns?.setOnPreferenceClickListener {
+            showDialog()
+            true;
+        }
+
 
         settingsToolbar.setNavigationOnClickListener {
             findNavController().navigate(R.id.action_settingsFragment_to_galleryFragment)
@@ -41,5 +69,34 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+    fun showDialog() {
+        val galeryPreference = PreferenceManager.getDefaultSharedPreferences(context)
+
+        var defaultItem = -1
+        if (galeryColumns == 4){
+            defaultItem = 0
+        }else if (galeryColumns == 5){
+            defaultItem = 1
+        }else if (galeryColumns == 6){
+            defaultItem = 2
+        }
+
+        val listItems = arrayOf("4", "5", "6")
+        val mBuilder = AlertDialog.Builder(activity)
+        mBuilder.setTitle(getString(R.string.settings_gallery_columns_option))
+        mBuilder.setSingleChoiceItems(listItems, defaultItem) { dialogInterface, i ->
+            galeryPreference.edit().putString("gallery^advanced.galleryColumns",listItems[i]).apply()
+            dialogInterface.dismiss()
+        }
+        mBuilder.setNeutralButton(getString(R.string.settings_gallery_columns_cancel)) { dialog, which ->
+            dialog.cancel()
+        }
+
+        val mDialog = mBuilder.create()
+        mDialog.show()
+
+
     }
 }
