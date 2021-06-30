dfaee6731fc4bcb47a72868c5b7bf3ea84f85855
Leon Latsch <dev.leonlatsch@gmail.com>
2021-06-30 16:56:43 +0200

    update lifecycle components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,8 @@ dependencies {
     val hiltVersion = "2.31-alpha"
 
     // Architectural Components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Room
