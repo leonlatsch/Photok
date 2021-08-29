bc5f44be40ef91e41f06aa1bfad64c9ae043521c
Leon Latsch <dev.leonlatsch@gmail.com>
2021-08-29 17:45:40 +0200

    cleanup  gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import java.util.*
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     val kotlinVersion = "1.4.21"
     repositories {
@@ -13,8 +10,6 @@ buildscript {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
         classpath("com.jaredsburrows:gradle-license-plugin:0.8.90")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
