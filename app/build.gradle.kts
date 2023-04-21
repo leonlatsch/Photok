84a0850ce6fd59a0536aee57881979fddb09399b
Leon Latsch <dev.leonlatsch@gmail.com>
2023-04-21 09:19:54 +0200

    update compose to 2023.04.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -120,7 +120,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.7.0")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.04.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.04.01"))
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
