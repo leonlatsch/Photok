1917bffc46440aef38c11fde82ae434dce3248b7
Leon Latsch <50983026+leonlatsch@users.noreply.github.com>
2023-06-01 10:56:12 +0200

    Increase feature version code

--- app/src/main/java/dev/leonlatsch/photok/news/newfeatures/ui/NewFeaturesDialog.kt
+++ app/src/main/java/dev/leonlatsch/photok/news/newfeatures/ui/NewFeaturesDialog.kt
@@ -30,7 +30,7 @@ import dev.leonlatsch.photok.uicomponnets.bindings.BindableDialogFragment
  * Increase for this Dialog to show on the next update.
  * @see dev.leonlatsch.photok.gallery.ui.GalleryViewModel.runIfNews
  */
-const val FEATURE_VERSION_CODE = 2
+const val FEATURE_VERSION_CODE = 3
 
 /**
  * Dialog for displaying new features.
