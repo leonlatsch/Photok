165d7bb2e7c1c66e51976d14b11eced14167a0f4
Leon Latsch <dev.leonlatsch@gmail.com>
2023-11-16 23:37:30 +0100

    reimplement old gallery ui

--- app/src/main/java/dev/leonlatsch/photok/cgallery/ui/compose/GalleryContent.kt
+++ app/src/main/java/dev/leonlatsch/photok/cgallery/ui/compose/GalleryContent.kt
@@ -17,21 +17,74 @@
 package dev.leonlatsch.photok.cgallery.ui.compose
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.leonlatsch.photok.R
 import dev.leonlatsch.photok.cgallery.ui.GalleryUiEvent
 import dev.leonlatsch.photok.cgallery.ui.GalleryUiState
 import dev.leonlatsch.photok.cgallery.ui.PhotoTile
 import dev.leonlatsch.photok.model.database.entity.PhotoType
+import java.util.UUID
 
 @Composable
 fun GalleryContent(uiState: GalleryUiState.Content, handleUiEvent: (GalleryUiEvent) -> Unit) {
-    Column {
-        Text(text = "${stringResource(R.string.gallery_all_photos_label)} (${uiState.photos.size})")
-        PhotosGrid(uiState.photos, uiState.selectionMode, handleUiEvent)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.padding(vertical = dimensionResource(R.dimen.small_gap)),
+                backgroundColor = Color.Transparent,
+                elevation = 0.dp,
+                title = {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        fontFamily = FontFamily(Font(R.font.lobster_regular)),
+                        color = colorResource(R.color.appTitleColor),
+                        fontSize = 40.sp
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { TODO() }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_lock),
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = { TODO() }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_settings),
+                            contentDescription = null
+                        )
+                    }
+                })
+        }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                modifier = Modifier.padding(start = 2.dp),
+                text = "${stringResource(R.string.gallery_all_photos_label)} (${uiState.photos.size})"
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            PhotosGrid(uiState.photos, uiState.selectionMode, handleUiEvent)
+        }
     }
 }
 
@@ -42,11 +95,21 @@ fun GalleryContentPreview() {
         uiState = GalleryUiState.Content(
             selectionMode = true,
             listOf(
-                PhotoTile("", PhotoType.JPEG, "1"),
-                PhotoTile("", PhotoType.MP4, "2"),
-                PhotoTile("", PhotoType.GIF, "3"),
-                PhotoTile("", PhotoType.MPEG, "4"),
-                PhotoTile("", PhotoType.PNG, "5"),
+                PhotoTile("", PhotoType.JPEG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.MP4, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.GIF, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.MPEG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
+                PhotoTile("", PhotoType.PNG, UUID.randomUUID().toString()),
             )
         ),
         handleUiEvent = {}
