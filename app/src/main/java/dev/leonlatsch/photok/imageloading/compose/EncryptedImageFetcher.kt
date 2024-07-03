e98633e83965d09b509abbfa91b27f462c792b7b
Leon Latsch <dev.leonlatsch@gmail.com>
2024-07-03 10:52:03 +0200

    Fix: Play gifs. - Using coils gif support does not work for loading drawables. Implemented my own solution for it

--- app/src/main/java/dev/leonlatsch/photok/imageloading/compose/EncryptedImageFetcher.kt
+++ app/src/main/java/dev/leonlatsch/photok/imageloading/compose/EncryptedImageFetcher.kt
@@ -19,12 +19,17 @@ package dev.leonlatsch.photok.imageloading.compose
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.graphics.Movie
+import android.os.Build
 import androidx.core.graphics.drawable.toDrawable
 import coil.decode.DataSource
+import coil.drawable.MovieDrawable
 import coil.fetch.DrawableResult
 import coil.fetch.FetchResult
 import coil.fetch.Fetcher
 import dev.leonlatsch.photok.imageloading.compose.model.EncryptedImageRequestData
+import dev.leonlatsch.photok.model.database.entity.PhotoType
 import dev.leonlatsch.photok.model.io.EncryptedStorageManager
 
 /**
@@ -38,15 +43,28 @@ class EncryptedImageFetcher(
     private val resources: Resources,
 ) : Fetcher {
 
+    @Suppress("DEPRECATION") // ImageDecoder is only available from API 28
     override suspend fun fetch(): FetchResult? {
         val inputStream = encryptedStorageManager.internalOpenEncryptedFileInput(requestData.internalFileName)
         inputStream ?: return null
 
-        val bitmap: Bitmap? = BitmapFactory.decodeStream(inputStream)
-        bitmap ?: return null
+        val drawable = if (requestData.mimeType == PhotoType.GIF.mimeType && requestData.playGif) {
+            if (Build.VERSION.SDK_INT >= 31) {
+                val bytes = inputStream.readBytes()
+                val source = ImageDecoder.createSource(bytes)
+                ImageDecoder.decodeDrawable(source)
+            } else {
+                val movie = Movie.decodeStream(inputStream)
+                MovieDrawable(movie)
+            }
+        } else {
+            val bitmap: Bitmap? = BitmapFactory.decodeStream(inputStream)
+            bitmap ?: return null
+            bitmap.toDrawable(resources)
+        }
 
         return DrawableResult(
-            drawable = bitmap.toDrawable(resources),
+            drawable = drawable,
             isSampled = false,
             dataSource = DataSource.DISK,
         )
