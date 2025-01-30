e5db2552d7b7753e98e46c76fc694fa94ab04669
Leon Latsch <git@leonlatsch.dev>
2025-01-30 11:29:39 +0100

    Update deps. Exclude broken deps. See https://issuetracker.google.com/u/1/issues/386304679?pli=1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion = "2.0.20"
+    val kotlinVersion = "2.0.20"// Don't update. See https://issuetracker.google.com/u/1/issues/386304679?pli=1
     repositories {
         google()
         mavenCentral()
@@ -8,8 +8,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.6")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.55")
         classpath("com.jaredsburrows:gradle-license-plugin:0.9.8")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
