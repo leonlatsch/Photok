5d024acb9666a795739c66f4aa75a68d72845915
Leon Latsch <git@leonlatsch.dev>
2025-02-08 13:32:10 +0100

    Rework menu to be a bottom sheet

--- app/src/main/java/dev/leonlatsch/photok/gallery/ui/components/MagicFabMenu.kt
+++ app/src/main/java/dev/leonlatsch/photok/gallery/ui/components/MagicFabMenu.kt
@@ -24,29 +24,50 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
@@ -54,22 +75,37 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastJoinToString
+import androidx.room.util.TableInfo
 import dev.leonlatsch.photok.R
 import dev.leonlatsch.photok.ui.theme.AppTheme
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
-private val EnterAnimSpec = scaleIn(
-    transformOrigin = TransformOrigin(1f, 1f)
-) + fadeIn()
-
-private val ExitAnimSpec = scaleOut(
-    transformOrigin = TransformOrigin(1f, 1f)
-) + fadeOut()
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MagicFabMenu(
     openState: MutableState<Boolean>,
     modifier: Modifier = Modifier,
+) {
+    if (openState.value) {
+        ModalBottomSheet(
+            modifier = modifier,
+            onDismissRequest = { openState.value = false },
+            dragHandle = {
+                BottomSheetDefaults.DragHandle()
+            }
+        ) {
+            ImportMenuDialogContent(
+                openState = openState,
+            )
+        }
+    }
+}
+
+@Composable
+fun ImportMenuDialogContent(
+    openState: MutableState<Boolean>,
+    modifier: Modifier = Modifier
 ) {
     val importNewFilesLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.PickMultipleVisualMedia()) {
@@ -78,98 +114,81 @@ fun MagicFabMenu(
         }
     val restoreBackupLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {
+            openState.value = false
             Timber.d(it.toString())
         }
 
-    AnimatedVisibility(
-        visible = openState.value,
-        enter = EnterAnimSpec,
-        exit = ExitAnimSpec,
+    Column(
+        modifier = modifier.padding(vertical = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Box(
-            modifier = modifier
-                .fillMaxSize()
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null,
-                ) { openState.value = false },
-            contentAlignment = Alignment.BottomCenter,
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp)
-                    .clip(RoundedCornerShape(12.dp))
-                    .background(colorResource(R.color.cardBackground))
-                    .padding(12.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                MagicFabMenuItem(
-                    menuOpenState = openState,
-                    title = "Import new Files",
-                    description = "Import files from your gallery.",
-                    icon = {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_add),
-                            contentDescription = null,
-                            tint = colorResource(R.color.textColor),
-                        )
-                    },
-                    onClick = {
-                        importNewFilesLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
-                    },
+        ImportMenuItem(
+            text = "Import new files",
+            description = "Import photos and videos from your gallery",
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_add),
+                    contentDescription = null
                 )
-                MagicFabMenuItem(
-                    menuOpenState = openState,
-                    title = "Restore backup",
-                    description = "Restore a backup. The restored backup will be added to your current files.",
-                    icon = {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_backup_restore),
-                            contentDescription = null,
-                            tint = colorResource(R.color.colorComplementary),
-                        )
-                    },
-                    onClick = {
-                        restoreBackupLauncher.launch(arrayOf("application/zip"))
-                    },
+            },
+            onClick = {
+                importNewFilesLauncher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo)
                 )
             }
-        }
+        )
+
+        HorizontalDivider()
+
+        ImportMenuItem(
+            text = "Restore a backup",
+            description = "Restore a photok backup. Restored files will be added to your vault",
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_backup_restore),
+                    contentDescription = null
+                )
+            },
+            onClick = {
+                restoreBackupLauncher.launch(emptyArray())
+            }
+        )
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
-private fun MagicFabMenuItem(
-    menuOpenState: MutableState<Boolean>,
-    title: String,
+fun ImportMenuItem(
+    text: String,
     description: String,
     icon: @Composable () -> Unit,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        modifier = modifier.clickable(role = Role.Button) {
-            menuOpenState.value = false
-            onClick()
-        },
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    Column(
+        modifier = modifier
+            .clickable { onClick() }
+            .padding(horizontal = 22.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
-        icon()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            icon()
 
-        Column {
             Text(
-                text = title,
-                style = MaterialTheme.typography.titleSmall,
-                fontWeight = FontWeight.Bold,
-                color = colorResource(R.color.textColor),
-            )
-            Text(
-                text = description,
-                style = MaterialTheme.typography.bodyMedium,
-                color = colorResource(R.color.secondaryTextColor),
+                text = text,
+                style = MaterialTheme.typography.headlineSmall,
             )
         }
+
+
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+        )
     }
 }
 
@@ -179,7 +198,7 @@ private fun Preview() {
     val openState = remember { mutableStateOf(true) }
     AppTheme {
         Surface {
-            MagicFabMenu(openState)
+            ImportMenuDialogContent(openState)
         }
     }
 }
