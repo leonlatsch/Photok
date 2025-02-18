b0d9516bb0ee6862f8aabb9db9be45ea0cbcf844
Leon Latsch <50983026+leonlatsch@users.noreply.github.com>
2025-02-18 09:25:56 +0100

    Bug: Fix crash with very big images (#419)
    **Description:**
    
    Fixes a crash when displaying very big images.
    
    Closes #403 
    
    Explanation:
    Normal downsampling is currently not possible. Because of the nature of
    AES/GCM (seems like changing the IV to 12 bytes fixes this, but this is
    a much bigger topic)
    
    Applied solution: Read all bytes into memory and provide as a buffered
    source from memory. This allows downsampling done by coil, since its
    already decrypted in memory.

--- app/src/main/java/dev/leonlatsch/photok/other/extensions/WindowExtensions.kt
+++ app/src/main/java/dev/leonlatsch/photok/other/extensions/WindowExtensions.kt
@@ -17,11 +17,11 @@
 package dev.leonlatsch.photok.other.extensions
 
 import android.os.Build
+import android.util.DisplayMetrics
 import android.view.View
 import android.view.Window
 import android.view.WindowInsets
-import androidx.annotation.ColorRes
-import androidx.core.content.ContextCompat
+import android.view.WindowManager
 
 /**
  * Compat method for adding a [visibilityListener] to the system ui.
@@ -41,3 +41,17 @@ fun Window.addSystemUIVisibilityListener(visibilityListener: (Boolean) -> Unit) 
         }
     }
 }
+
+/**
+ * Get the device screen size. Uses appropriate API for different android versions.
+ */
+fun WindowManager.getCompatScreenSize(): Pair<Int, Int> =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val bounds = this.currentWindowMetrics.bounds
+        Pair(bounds.width(), bounds.height())
+    } else {
+        val metrics = DisplayMetrics()
+        @Suppress("DEPRECATION")
+        this.defaultDisplay.getMetrics(metrics)
+        Pair(metrics.widthPixels, metrics.heightPixels)
+    }
