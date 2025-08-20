5ea7957a682153d576163e479f0ed89c547136b0
Leon Latsch <git@leonlatsch.dev>
2025-08-20 21:26:09 +0200

    Create PermissionUtils.kt and move requestInSettings

--- app/src/main/java/dev/leonlatsch/photok/other/PermissionUtils.kt
+++ app/src/main/java/dev/leonlatsch/photok/other/PermissionUtils.kt
@@ -17,20 +17,8 @@
 package dev.leonlatsch.photok.other
 
 import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.provider.Settings
-import androidx.core.app.NotificationManagerCompat
-import dev.leonlatsch.photok.other.extensions.startActivityAndIgnoreTimer
+import androidx.core.app.ActivityCompat
 
-fun Context.areNotificationsEnabled(): Boolean {
-    return NotificationManagerCompat.from(this).areNotificationsEnabled()
-}
-
-fun Activity.openNotificationSettings() {
-    val intent = Intent().apply {
-        action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
-        putExtra(Settings.EXTRA_APP_PACKAGE, this@openNotificationSettings.packageName)
-    }
-    startActivityAndIgnoreTimer(intent, this)
+fun Activity.requestInSettings(permission: String): Boolean {
+    return ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
 }
