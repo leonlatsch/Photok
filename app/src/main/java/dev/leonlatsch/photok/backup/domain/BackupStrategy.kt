03b976299ed01dd1452324abc1505d57f988a3a7
Leon Latsch <git@leonlatsch.dev>
2025-09-16 11:16:54 +0200

    feat: Use specific title for Unencrypted Backup
    This commit updates the title for the unencrypted backup process to "Decrypting to ZIP…" for better clarity.
    
    It also introduces a new string resource `migration_error_extracting_title` and updates translations for various languages.
    The `BackupBottomSheetDialogFragment` now uses the `title` property from the `BackupStrategy.Name` enum to set the processing label.

--- app/src/main/java/dev/leonlatsch/photok/backup/domain/BackupStrategy.kt
+++ app/src/main/java/dev/leonlatsch/photok/backup/domain/BackupStrategy.kt
@@ -16,15 +16,16 @@
 
 package dev.leonlatsch.photok.backup.domain
 
+import dev.leonlatsch.photok.R
 import dev.leonlatsch.photok.model.database.entity.Photo
 import java.util.zip.ZipOutputStream
 
 interface BackupStrategy {
 
-    enum class Name {
-        Default,
-        Legacy,
-        UnEncrypted;
+    enum class Name(val title: Int) {
+        Default(R.string.backup_processing_title),
+        Legacy(R.string.backup_processing_title),
+        UnEncrypted(R.string.migration_error_extracting_title);
     }
 
     suspend fun writePhotoToBackup(
