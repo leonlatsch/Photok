3730e86a2bed0e767def087d821fda38c27867a6
Leon Latsch <git@leonlatsch.dev>
2025-09-24 12:22:56 +0200

    Style: Improve dropdown menu design
    Apply rounded corners to the DropdownMenu in MultiSelectionMenu.

--- app/src/main/java/dev/leonlatsch/photok/ui/components/MultiSelectionMenu.kt
+++ app/src/main/java/dev/leonlatsch/photok/ui/components/MultiSelectionMenu.kt
@@ -26,12 +26,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -99,6 +101,7 @@ fun MultiSelectionMenu(
                         .matchParentSize()
                 ) {
                     DropdownMenu(
+                        shape = RoundedCornerShape(16.dp),
                         expanded = multiSelectionState.showMore.value,
                         onDismissRequest = {
                             multiSelectionState.dismissMore()
