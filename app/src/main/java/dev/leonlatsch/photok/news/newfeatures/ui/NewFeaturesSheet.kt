e7b1d0e1e86d9e0b00e1dde63544de1a7583d946
Leon Latsch <git@leonlatsch.dev>
2026-02-19 23:56:55 +0100

    Improved layout of news dialog for screen sizes

--- app/src/main/java/dev/leonlatsch/photok/news/newfeatures/ui/NewFeaturesSheet.kt
+++ app/src/main/java/dev/leonlatsch/photok/news/newfeatures/ui/NewFeaturesSheet.kt
@@ -17,8 +17,9 @@
 package dev.leonlatsch.photok.news.newfeatures.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -28,15 +29,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -45,15 +44,20 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.leonlatsch.photok.BuildConfig
@@ -62,6 +66,7 @@ import dev.leonlatsch.photok.other.openUrl
 import dev.leonlatsch.photok.settings.ui.compose.LocalConfig
 import dev.leonlatsch.photok.ui.components.AppName
 import dev.leonlatsch.photok.ui.theme.AppTheme
+import kotlinx.coroutines.launch
 
 
 data class NewFeature(
@@ -108,14 +113,15 @@ const val FEATURE_VERSION_CODE = 12
 fun NewFeaturesSheet() {
     val config = LocalConfig.current
 
-    var visible by remember { mutableStateOf(false) }
+    val isPreview = LocalInspectionMode.current
+    var visible by remember { mutableStateOf(isPreview) }
 
     LaunchedEffect(Unit) {
         config ?: return@LaunchedEffect
 
         if (config.systemLastFeatureVersionCode < FEATURE_VERSION_CODE) {
             visible = true
-            config.systemLastFeatureVersionCode = FEATURE_VERSION_CODE
+//            config.systemLastFeatureVersionCode = FEATURE_VERSION_CODE
         }
     }
 
@@ -128,82 +134,82 @@ fun NewFeaturesSheet() {
             sheetState = state,
             onDismissRequest = { visible = false },
             dragHandle = null,
+            sheetGesturesEnabled = false,
+            containerColor = Color.Transparent
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .verticalScroll(rememberScrollState())
-                    .padding(20.dp)
-                    .fillMaxWidth()
+            Surface(
+                shape = BottomSheetDefaults.ExpandedShape,
+                color = BottomSheetDefaults.ContainerColor,
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.ic_party_popper),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(60.dp)
-                            .graphicsLayer {
-                                this.rotationY = 180f
-                            }
-
-                    )
-
+                Box {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.padding(horizontal = 20.dp)
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .verticalScroll(rememberScrollState())
+                            .padding(bottom = 100.dp)
                     ) {
-                        Text(
-                            text = stringResource(R.string.news_new_in_title)
+                        Image(
+                            painterResource(R.drawable.app_icon),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(80.dp)
                         )
+
                         AppName()
 
                         Text(
-                            text = BuildConfig.VERSION_NAME
+                            text = BuildConfig.VERSION_NAME,
+                            style = MaterialTheme.typography.titleLarge,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
                         )
-                    }
 
-                    Image(
-                        painter = painterResource(R.drawable.ic_party_popper),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(60.dp)
-                    )
-                }
+                        Spacer(modifier = Modifier.height(20.dp))
 
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Card(
-                    shape = RoundedCornerShape(24.dp),
-                    border = BorderStroke(width = DividerDefaults.Thickness, color = DividerDefaults.color),
-                ) {
-                    Column(
-                        modifier = Modifier.padding(12.dp)
-                    ) {
                         for (feature in NewFeatures) {
                             NewFeatureRow(
                                 feature = feature,
                             )
-
-                            if (feature != NewFeatures.last()) {
-                                Spacer(modifier = Modifier.height(5.dp))
-                                HorizontalDivider()
-                                Spacer(modifier = Modifier.height(5.dp))
-                            }
+                            Spacer(modifier = Modifier.height(10.dp))
                         }
                     }
-                }
 
-                val context = LocalContext.current
-                val changelogUrl = stringResource(R.string.news_changelog_url)
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .background(BottomSheetDefaults.ContainerColor)
+                            .padding(horizontal = 20.dp)
+                    ) {
+                        val context = LocalContext.current
+                        val changelogUrl = stringResource(R.string.news_changelog_url)
 
-                TextButton(
-                    onClick = { context.openUrl(changelogUrl) }
-                ) {
-                    Text(
-                        text = stringResource(R.string.news_view_changelog)
-                    )
+                        TextButton(
+                            onClick = { context.openUrl(changelogUrl) }
+                        ) {
+                            Text(
+                                text = stringResource(R.string.news_view_changelog)
+                            )
+                        }
+
+                        val scope = rememberCoroutineScope()
+
+                        Button(
+                            onClick = {
+                                scope.launch {
+                                    state.hide()
+                                }.invokeOnCompletion {
+                                    visible = false
+                                }
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = stringResource(R.string.common_ok)
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -216,7 +222,7 @@ private fun NewFeatureRow(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier
+        modifier = modifier.fillMaxWidth()
     ) {
         Text(
             text = feature.emoji,
@@ -240,12 +246,12 @@ private fun NewFeatureRow(
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@PreviewLightDark
+@Preview(showSystemUi = true)
+@Preview(showSystemUi = true, device = Devices.NEXUS_5)
+@PreviewScreenSizes
 @Composable
 private fun Preview() {
     AppTheme() {
-        Scaffold() {
-            NewFeaturesSheet()
-        }
+        NewFeaturesSheet()
     }
 }
