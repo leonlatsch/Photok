b1dce790e7ec391908ace9b5175c07ca81770792
Leon Latsch <git@leonlatsch.dev>
2026-09-12 00:31:10 +0200

    Fix FOSS build in release variant

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     // SharedPreferences edit extension
     implementation("androidx.core:core-ktx:1.16.0")
 
+    // AppCompat (drawables in res/drawable reference ?attr/colorControlNormal)
+    implementation("androidx.appcompat:appcompat:1.7.1")
+
     // Timber Logging
     implementation("com.jakewharton.timber:timber:5.0.1")
 
