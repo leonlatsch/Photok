76ce077ca9a37d723551658f2904da15b4fe96f3
Leon Latsch <git@leonlatsch.dev>
2026-09-19 02:59:15 +0200

    use icon not "ZIP"

--- app/src/main/java/dev/leonlatsch/photok/backup/ui/restore/BackupFileHeader.kt
+++ app/src/main/java/dev/leonlatsch/photok/backup/ui/restore/BackupFileHeader.kt
@@ -5,18 +5,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import dev.leonlatsch.photok.R
 import dev.leonlatsch.photok.ui.theme.AppTheme
 
 @Composable
@@ -39,13 +40,10 @@ fun BackupFileHeader(
                 shape = RoundedCornerShape(8.dp),
                 tonalElevation = 10.dp,
             ) {
-                Text(
-                    text = "ZIP",
-                    fontWeight = FontWeight.Black,
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Monospace,
-                    modifier = Modifier
-                        .padding(10.dp)
+                Icon(
+                    painter = painterResource(R.drawable.ic_zip),
+                    contentDescription = "ZIP",
+                    modifier = Modifier.padding(10.dp)
                 )
             }
 
